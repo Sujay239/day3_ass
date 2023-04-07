cf9274de88aa408870dca8e035be279d9c3366e4
Sujay239 <125623523+Sujay239@users.noreply.github.com>
2023-04-07 19:05:31 +0530

    Largest.c

---
+++ Largestnumber.c
@@ -0,0 +1,15 @@
+#include<studio.h>
+int main(){
+int a, b , c;
+printf("enter a,b,c :");
+scanf("%d%d%d\n", &a,&b,&c);
+
+if(a>b && a>c){
+printf("The largest number is : %d\n", a);
+}else if(b>a && b>c){
+printf ("The largest number is : %d\n", b);
+}else{
+printf ("The largest number is: %d\n", c);
+}
+return 0;
+}
